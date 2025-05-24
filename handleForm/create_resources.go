511896fd0b15910a610d560c99350fda0595c7e9
kiril <kirilpoposki@hotmail.com>
2025-05-24 19:09:32 +0200

    Add create resource for project and testing

--- handleForm/create_resources.go
+++ handleForm/create_resources.go
@@ -2,10 +2,16 @@ package handleform
 
 import "os"
 
-func (p Project) create() error {
+func (p Project) Create() error {
 	err := os.Mkdir(p.Path, os.ModePerm)
 	if err != nil {
 		return err
+	}
+	if p.InfraEnabled {
+		err = os.Mkdir(p.Path+"/infra", os.ModePerm)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
--- tests/create_resources_test.go
+++ tests/create_resources_test.go
@@ -1,7 +1,31 @@
 package tests
 
-import "testing"
+import (
+	handleform "3ML/handleForm"
+	"os"
+	"testing"
+)
 
-func TestCreateResources(t *testing.T) {
-	t.Skip("Skipping create resources test as it requires user input and environment setup")
+func TestCreateResourceProject(t *testing.T) {
+	proj, err := handleform.SetupProject()
+	if err != nil {
+		t.Fatalf("Failed to setup project: %v", err)
+	}
+	proj.Name = "test_project"
+	proj.Path, err = os.Getwd()
+	proj.Path += "/" + proj.Name
+	if err != nil {
+		t.Fatalf("Failed to get current working directory: %v", err)
+	}
+	proj.InfraEnabled = true
+	err = proj.Create()
+	if err != nil {
+		t.Fatalf("Failed to create project: %v", err)
+	}
+	os.RemoveAll(proj.Path)
+	proj.Path = "/some_path" // Set a temporary path for testing
+	err = proj.Create()
+	if err == nil {
+		t.Fatalf("Expected error when creating project with existing path, got nil")
+	}
 }
